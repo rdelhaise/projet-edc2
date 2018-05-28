3dd477daf6a1933be0cefca76d8a25668142bdd9
Rémy DELHAISE <rdelhaise@misyl-services.com>
2018-05-28 16:10:16 +0200

    refactoring

--- src/com/epsi/book/application/AllBooksAlreadyReturnedException.java
+++
@@ -1,8 +0,0 @@
-package com.epsi.book.application;
-
-public class AllBooksAlreadyReturnedException extends Exception {
-
-	public AllBooksAlreadyReturnedException() {
-		System.out.println("Tous les livres avec l'identifiant donné sont déjà retourné");
-	}
-}
